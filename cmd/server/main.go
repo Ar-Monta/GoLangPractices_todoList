f7e8d4b5b21820b419844b98dcb445fb2632cd97
Arshia Montakhabi <armoteamco@gmail.com>
2023-07-25 11:36:54 +0330

    [Add] Added reading database needed fields from .env file.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ArMo-Team/GoLangPractices_todoList/internal/app/todo"
 	"github.com/ArMo-Team/GoLangPractices_todoList/internal/infrastructure/persistence"
@@ -12,7 +15,20 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/todo-app-db")
+	// Load variables from .env file into environment variables
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+	}
+
+	// Access environment variables
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	// You can add password here by using /* dbPassword := os.Getenv("DB_PASSWORD") */
+
+	// Open connection to database
+	db, err := sql.Open("mysql", dbUser+"@tcp("+dbHost+":"+dbPort+")/"+os.Getenv("DB_NAME"))
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
 	}
